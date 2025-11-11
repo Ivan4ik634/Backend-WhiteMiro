d4ec028604f8fa0b7f646b0844f5220bc0612e44
Ivan Levadny <ivan@example.com>
2025-11-11 13:05:25 +0200

    add webhook stripe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
+
+  // Важное: подключаем raw body только для вебхука
+  app.use(
+    '/payment/webhook',
+    express.raw({ type: 'application/json' }),
+  );
+
   await app.listen(process.env.PORT ?? 4200);
 }
 bootstrap();
