a9cfd678c36c0f590ba5f8c2081d6a4229f783b7
Ivan Levadny <ivan@example.com>
2025-11-13 14:02:11 +0200

    fixedd

--- src/totp/totp.service.ts
+++ src/totp/totp.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import qrcode from 'qrcode';
+import * as qrcode from 'qrcode';
 import * as speakeasy from 'speakeasy';
 import { User } from 'src/shemes/User.scheme';
 
